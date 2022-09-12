d03d6c99774138e050de04d0de43a89d1d99263c
Carter McCall <Carter_McCall53@outlook.com>
2022-09-12 09:31:28 -0600

    Add score keeping abilities

--- RockPaperSissors/RockPaperSissors.cpp
+++ RockPaperSissors/RockPaperSissors.cpp
@@ -3,6 +3,8 @@
 *  8 September 2022
 *  Purpose:
 *		Create the rock paper sissors game
+*  Modified 12 September 2022:
+*		Add score keeping abilities
 */
 
 #include <iostream>
@@ -14,42 +16,57 @@ int main() {
 	string choices[] = { "Rock", "Paper", "Sissors" };
 	char repChar;
 	char charChoice;
-	int compChoice, userChoice;
+	int compChoice, userChoice,
+		wins = 0, losses = 0, ties = 0;
 	srand(int(time(0)));
 	do
 	{
 		compChoice = rand() % 3;
-		cout << "What would you like to play? (R|P|S): ";
-		for (int i = 1; i > 0;) {
-			cin >> input;
-			charChoice = tolower(input[0]);
-			if (charChoice == 'r') 
-			{
-				userChoice = 0;
-				i -= 1;
-			}
-			else if (charChoice == 'p')
-			{
-				userChoice = 1;
-				i -= 1;
-			}
-			else if (charChoice == 's')
-			{
-				userChoice = 2;
-				i -= 1;
-			}
-			else cout << "PLEASE CHOOSE A VALID PLAY";
+		cout << "\nYou have " << wins << " wins, " << losses << " losses, and "
+			<< ties << " ties!" << endl;
+		cout << "\nWhat would you like to play? (R|P|S): ";
+		cin >> input;
+		charChoice = tolower(input[0]);
+		if (charChoice == 'r') userChoice = 0;
+		else if (charChoice == 'p') userChoice = 1;
+		else if (charChoice == 's') userChoice = 2;
+		else
+		{
+			cout << "PLEASE CHOOSE A VALID PLAY";
+			continue;
 		}
 		cout << "\n";
-		if (userChoice == compChoice) cout << "You have tied the computer!" << endl;
-		else if (userChoice == 0 && compChoice == 2) cout << "You Win!" << endl;
-		else if (userChoice == 1 && compChoice == 0) cout << "You Win!" << endl;
-		else if (userChoice == 2 && compChoice == 1) cout << "You Win!" << endl;
-		else cout << "Computer Wins!" << endl;
+		if (userChoice == compChoice)
+		{
+			cout << "You have tied the computer!" << endl;
+			ties++;
+		}
+		else if (userChoice == 0 && compChoice == 2)
+		{
+			cout << "You Win!" << endl;
+			wins++;
+		}
+		else if (userChoice == 1 && compChoice == 0)
+		{
+			cout << "You Win!" << endl;
+			wins++;
+		}
+		else if (userChoice == 2 && compChoice == 1)
+		{
+			cout << "You Win!" << endl;
+			wins++;
+		}
+		else
+		{
+			cout << "Computer Wins!" << endl;
+			losses++;
+		}
 		cout << "You chose " << choices[userChoice] << " and the computer chose "
 			<< choices[compChoice] << endl;
 		cout << "Do you want to play again? (Y|N): ";
 		cin >> input;
 		repChar = tolower(input[0]);
 	} while (repChar == 'y');
+	cout << "\nYou ended with " << wins << " wins, " << losses << " losses, and "
+		<< ties << " ties!\n";
 }
